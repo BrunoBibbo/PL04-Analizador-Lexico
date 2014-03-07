6387152df5767824d54755eebd678a4d5d4fdbce
juanma3108 <alu0100597929@ull.edu.es>
2014-03-07 01:31:02 +0000

    añadidos mas test

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -10,4 +10,46 @@ suite('Tests', function() {
 	assert.equal(regexp.bexec(str), null);
   });
 
+  test('Parser comentario', function() {
+      var parse = make_parse();
+      var input = "/* holaaa */";
+      var str, tree;
+      try {
+	  tree = parse(input);
+	  str = JSON.stringify(tree, ['key', 'name', 'message',
+	      'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+      } catch (e) {
+	  str = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+                'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+      }
+      assert.deepEqual(str, "null");
+    });
+
+  test('Parser valor', function(){
+    var parse = make_parse();
+	var input = "var x = 15;";
+
+	var str, tree;
+    try {
+      tree = parse(input);
+      str = JSON.stringify(tree, ['key', 'name', 'message',
+           'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    } catch (e) {
+      str = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+              'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    }
+
+	assert.equal("{\n    \"value\": \"=\",\n    \"arity\": \"binary\",\n    \"first\": {\n        \"value\": \"x\",\n        \"arity\": \"name\"\n    },\n    \"second\": {\n        \"value\": 15,\n        \"arity\": \"literal\"\n    }\n}", str);
+  });
+
+  test('Error de entrada', function() 
+  {
+	window.onload=function() 
+	{
+		$("#INPUT").val("esto no es código");
+		main();
+		assert.match($("#OUTPUT").innerHTML, /(?:Type)?Error/i);
+	}
+  });
+
 });
